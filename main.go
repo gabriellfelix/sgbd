65966a625e867c72ee51bbf1b6f6cca6861d4f3e
Gabriel_LSBD_24146 <gabriellucassf08@gmail.com>
2023-05-16 15:38:22 -0300

    Insert: Operações na Memória Concluídas (#1)

--- main.go
+++ main.go
@@ -129,69 +129,7 @@ func ler_esp_livre_paginas(db_path string) []int {
 		}
 	}
 
-	// fmt.Println("Vetor Lido:",enderecos)
-
 	return esp_livre_paginas
-
-}
-
-func inserir_registro1(db_path string, enderecos []int, quant_paginas int, quant_bytes_por_pagina int) {
-	var registro string
-
-	for {
-		fmt.Println("Digite o Registro: ")
-		fmt.Scan(&registro)
-
-		if !(len(registro) > 5) {
-			break
-		} else {
-			registro = ""
-			fmt.Println("O Tamanho Máximo do Registro é de 5 Bytes")
-		}
-	}
-
-	quant_vazios := 0
-
-	for i := 0; i < len(enderecos); i++ {
-		if enderecos[i] == -1 {
-			quant_vazios += 1
-			if quant_vazios == len(registro) {
-				path := db_path + "/" + strconv.Itoa(i/quant_bytes_por_pagina) + ".txt"
-
-				pagina := i / quant_bytes_por_pagina
-				slot := (i % quant_bytes_por_pagina) + 1 - len(registro)
-
-				fmt.Println(fmt.Sprintf("Página %d, Slot %d", pagina, slot))
-
-				string_vetor := ""
-				for idx, letra := range registro {
-					pos_end := pagina*quant_bytes_por_pagina + slot + idx
-
-					// fmt.Println("pos ",pos_end)
-
-					string_vetor += string(letra) + "\n"
-
-					enderecos[pos_end] = slot
-				}
-
-				gravar_esp_livre_paginas(db_path, enderecos)
-				fmt.Println("enderecos ", enderecos)
-
-				arquivo, _ := os.OpenFile(path, os.O_APPEND|os.O_WRONLY, 0664)
-
-				arquivo.WriteString(string_vetor)
-
-				// ioutil.WriteFile(path, []byte(string_vetor), 0644)
-
-			}
-		} else {
-			quant_vazios = 0
-		}
-	}
-
-	// for _, i := range(a) {
-	//   fmt.Println(string(i))
-	// }
 
 }
 
@@ -227,12 +165,7 @@ func ler_registros_mem(db_path string, pagina int) []*Registro {
 	valor_registro := ""
 	tamanho_registro := 0
 
-	// path_pg := db_path+"/"+strconv.Itoa(pagina)+".txt"
-
 	ocupacao, valores_registros = ler_conteudo_pagina(db_path, pagina)
-
-	// fmt.Println("Ocupação ", ocupacao)
-	// fmt.Println("valores_Registros ", valores_registros)
 
 	for idx, _ := range ocupacao {
 
@@ -250,13 +183,6 @@ func ler_registros_mem(db_path string, pagina int) []*Registro {
 				}
 
 				registros = append(registros, &registro)
-
-				// fmt.Print("idx ", idx)
-				// fmt.Print(", val ", val)
-				// fmt.Print(", reg ", registro.conteudo)
-				// fmt.Print(", pg ", registro.pagina_id)
-				// fmt.Print(", slot ", registro.slot)
-				// fmt.Println(", tamanho ", registro.tamanho)
 
 				valor_registro = ""
 				tamanho_registro = 0
@@ -277,19 +203,147 @@ func criar_paginas(db_path string, paginas_ativas []int) []*Pagina {
 		fmt.Println(registros_pg)
 
 		pagina := Pagina{
-			id: paginas_ativas[idx],
+			id:        paginas_ativas[idx],
 			registros: registros_pg,
-			prox: nil,
+			prox:      nil,
 		}
 
 		paginas = append(paginas, &pagina)
 	}
 
-	for i := (len(paginas)-1); i>1; i--{
+	for i := (len(paginas) - 1); i > 1; i-- {
 		paginas[i-1].prox = paginas[i]
 	}
 
 	return paginas
+}
+
+func inserir_registro(db_path string, paginas_utilizadas *[]*Pagina, esp_livre_paginas []int) {
+	var registro_string string
+
+	for {
+		fmt.Println("Digite o Registro_string: ")
+		fmt.Scan(&registro_string)
+
+		if !(len(registro_string) > 5) {
+			break
+		} else {
+			registro_string = ""
+			fmt.Println("O Tamanho Máximo do Registro é de 5 Bytes")
+		}
+	}
+
+	quant_vazios := 0
+	slot_gravacao := -1
+
+	for _, pg_ativa := range *paginas_utilizadas {
+		if esp_livre_paginas[pg_ativa.id] >= len(registro_string) {
+			ocupacao_slots, _ := ler_conteudo_pagina(db_path, pg_ativa.id)
+
+			for i := 0; i < len(ocupacao_slots); i++ {
+				if ocupacao_slots[i] == -1 {
+					quant_vazios += 1
+					if quant_vazios == len(registro_string) {
+						slot_gravacao = i - len(registro_string) + 1
+						break
+					}
+				} else {
+					quant_vazios = 0
+				}
+			}
+
+			if slot_gravacao != -1 {
+				registro := Registro{
+					pagina_id: pg_ativa.id,
+					slot:      slot_gravacao,
+					tamanho:   len(registro_string),
+					conteudo:  registro_string,
+				}
+
+				pg_ativa.registros = append(pg_ativa.registros, &registro)
+
+				fmt.Println("pagina", pg_ativa.id)
+				fmt.Println("slot", slot_gravacao)
+				break
+			}
+
+		}
+
+	}
+
+	if slot_gravacao == -1 {
+		for idx, _ := range esp_livre_paginas {
+			if esp_livre_paginas[idx] == 5 {
+				slot_gravacao = 0
+
+				registro := Registro{
+					pagina_id: idx,
+					slot:      slot_gravacao,
+					tamanho:   len(registro_string),
+					conteudo:  registro_string,
+				}
+
+				var regs []*Registro
+
+				regs = append(regs, &registro)
+
+				pagina := Pagina{
+					id:        idx,
+					registros: regs,
+					prox:      nil,
+				}
+
+				(*paginas_utilizadas)[len(*paginas_utilizadas)-1].prox = &pagina
+
+				*paginas_utilizadas = append(*paginas_utilizadas, &pagina)
+
+				fmt.Println("pagina ", registro.pagina_id)
+
+				break
+			}
+		}
+	}
+
+	if slot_gravacao == -1 {
+		fmt.Println("Banco de Dados Cheio!!")
+	}
+
+	// for i := 0; i < len(enderecos); i++ {
+	// 	if enderecos[i] == -1 {
+	// 		quant_vazios += 1
+	// 		if quant_vazios == len(registro) {
+	// 			path := db_path + "/" + strconv.Itoa(i/quant_bytes_por_pagina) + ".txt"
+
+	// 			pagina := i / quant_bytes_por_pagina
+	// 			slot := (i % quant_bytes_por_pagina) + 1 - len(registro)
+
+	// 			fmt.Println(fmt.Sprintf("Página %d, Slot %d", pagina, slot))
+
+	// 			string_vetor := ""
+	// 			for idx, letra := range registro {
+	// 				pos_end := pagina*quant_bytes_por_pagina + slot + idx
+
+	// 				// fmt.Println("pos ",pos_end)
+
+	// 				string_vetor += string(letra) + "\n"
+
+	// 				enderecos[pos_end] = slot
+	// 			}
+
+	// 			gravar_esp_livre_paginas(db_path, enderecos)
+	// 			fmt.Println("enderecos ", enderecos)
+
+	// 			arquivo, _ := os.OpenFile(path, os.O_APPEND|os.O_WRONLY, 0664)
+
+	// 			arquivo.WriteString(string_vetor)
+
+	// 			// ioutil.WriteFile(path, []byte(string_vetor), 0644)
+
+	// 		}
+	// 	} else {
+	// 		quant_vazios = 0
+	// 	}
+
 }
 
 func main() {
@@ -308,16 +362,22 @@ func main() {
 
 	fmt.Println(esp_livre_paginas)
 
-	for _, i := range paginas_utilizadas{
+	for _, i := range paginas_utilizadas {
 		fmt.Println(i.id)
 
-		for _, reg := range i.registros{
+		for _, reg := range i.registros {
 			fmt.Println(reg.conteudo)
 		}
 	}
 
-	// inserir_registro1(db_path, enderecos, QUANT_PAGINAS, QUANT_BYTES_POR_PAGINA)
+	inserir_registro(DB_PATH, &paginas_utilizadas, esp_livre_paginas)
 
-	//  fmt.Scan(&i)
-	// fmt.Println("Hello, World!", i, len(i))
+	for _, i := range paginas_utilizadas {
+		fmt.Println(i.id)
+
+		for _, reg := range i.registros {
+			fmt.Println(reg.conteudo)
+		}
+	}
+
 }
